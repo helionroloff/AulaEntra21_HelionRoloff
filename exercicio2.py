b1706d522581b3160a6c5c60447080b528d4c0a5
helionroloff <60901788+helionroloff@users.noreply.github.com>
2020-09-17 14:13:54 -0300

    Add files via upload

---
+++ exercicio2.py
@@ -0,0 +1,19 @@
+
+
+# 2) Crie uma lista com 10 números qualquer.
+# 
+# Usando as funções da lista responda:
+# 
+# Quantos itens tem a lista?
+# Qual é o número maior?
+# Qual é o número menor?
+# Qual é o resultado da soma da lista?
+# 
+# 
+# 
+lista=[33,5,12,8,60,3,2,6,25,31]
+print(f'a lista tem {len(lista)} itens')
+print(f'o maior número é {max(lista)}')
+print(f'o menor número é {min(lista)}')
+print(f'a soma de todos os valores da lista é {sum(lista)}')
+
